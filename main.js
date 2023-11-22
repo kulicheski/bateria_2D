93e0eec3f78f50e934dd2912f74a266b5fe5fc50
kulicheski <132467657+kulicheski@users.noreply.github.com>
2023-11-22 10:36:10 -0300

    ...

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function(){
-    document.querySelector(#som_tecla_pum).play();
-}
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+    }
+    document.querySelector('.tecla_pom') .onclick = tocaSomPom;
